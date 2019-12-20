e0306b0029b77f16a53421e9389d46c5c9dbf2ee
paibamboo <gavinsitti@hotmail.com>
2019-12-20 09:42:18 +0700

    style: fix lint

--- src/helpers/deepAssign.ts
+++ src/helpers/deepAssign.ts
@@ -1,12 +1,14 @@
-function isObject(item) {
+function isObject(item: any): item is Record<string | number, any> {
   return (item && typeof item === "object" && !Array.isArray(item));
 }
 
 // from https://stackoverflow.com/a/34749873/5561369
 // should be used with care, you should make sure that target and sources are simple objects
 // see https://stackoverflow.com/a/28248548/5561369
-export function deepAssign(target, ...sources) {
-  if (!sources.length) return target;
+export function deepAssign(target: any, ...sources: any[]): any {
+  if (!sources.length) {
+    return target;
+  }
   const source = sources.shift();
 
   if (isObject(target) && isObject(source)) {
