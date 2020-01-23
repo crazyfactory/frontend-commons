1e7c187dde200481264e8a0305881e7ccf26d2d4
paibamboo <gavinsitti@hotmail.com>
2020-01-23 15:45:30 +0700

    style: fix lint

--- src/helpers/decodeJwt.test.ts
+++ src/helpers/decodeJwt.test.ts
@@ -5,22 +5,22 @@ describe("decodeJwt", () => {
     const token = "eyJjdHkiOiJKV1QiLCJraWQiOiJkZWZhdWx0IiwiYWxnIjoiSFMyNTYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJDRiIsImV4cCI6MTU3OTc1OTc2MywiaWF0IjoxNTc5NzU4ODYzLCJjdXN0b21lcklkIjoxLCJyb2xlIjoiYWRtaW4iLCJzY29wZXMiOlsidXNlciIsImFkbWluIl0sImVtYWlsIjoiYWRtaW5AZXhhbXBsZS5jb20iLCJmaXJzdE5hbWUiOiJBZG1pbiIsImxhc3ROYW1lIjoiQWRtaW4iLCJjdXJyZW5jeSI6IkVVUiIsImxhbmd1YWdlQ29kZSI6ImVuLUdCIiwibWVhc3VyZW1lbnRzIjoibW1cL2NtIiwiY291bnRyeSI6IkRFIn0.v59sJ6yjIhS3o8yi2drXYFzbcFzVNncMU60cHvWv7qI";
     const decoded = decodeJwt(token);
     expect(decoded).toEqual({
-      iss: "CF",
-      exp: 1579759763,
-      iat: 1579758863,
+      country: "DE",
+      currency: "EUR",
       customerId: 1,
+      email: "admin@example.com",
+      exp: 1579759763,
+      firstName: "Admin",
+      iat: 1579758863,
+      iss: "CF",
+      languageCode: "en-GB",
+      lastName: "Admin",
+      measurements: "mm/cm",
       role: "admin",
       scopes: [
         "user",
         "admin"
-      ],
-      email: "admin@example.com",
-      firstName: "Admin",
-      lastName: "Admin",
-      currency: "EUR",
-      languageCode: "en-GB",
-      measurements: "mm/cm",
-      country: "DE"
+      ]
     });
   });
 });
