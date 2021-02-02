c3f3a38b8a090a9e78764874ab05882f63355d34
paibamboo <gavinsitti@hotmail.com>
2021-02-02 16:29:10 +0700

    feat: add legacy breakpoints

---
+++ src/constants/LegacyBreakpoints.ts
@@ -0,0 +1,9 @@
+// tslint:disable:object-literal-sort-keys
+export const LegacyBreakpoints = {
+    SPHONE: 360,
+    PHONE: 480,
+    MEDIUM: 640,
+    TABLET: 768,
+    LARGE: 1024,
+    XLARGE: 1280
+};
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export {Color} from "./constants/Color";
 export {CommonCss} from "./constants/CommonCss";
 export {FontSize} from "./constants/FontSize";
 export {FontWeight} from "./constants/FontWeight";
+export {LegacyBreakpoints} from "./constants/LegacyBreakpoints";
 export {MAX_WIDTH} from "./constants/MaxWidth";
 export {ZIndex} from "./constants/ZIndex";
 export {AddEventListener} from "./helpers/AddEventListener";
